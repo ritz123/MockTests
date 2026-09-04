0b50531df3026234ffd0234295e88e77218898fa
Biplab Sarkar <biplab.sarkar@nokia.com>
2026-09-04 22:40:35 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["127.0.0.1", "10.*.*.*", "172.*.*.*", "192.168.*.*"],
+  allowedDevOrigins: ["titan.local","127.0.0.1", "10.*.*.*", "172.*.*.*", "192.168.*.*"],
 };
 
 export default nextConfig;
